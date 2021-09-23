92c888cfdc1b95827d1ff2ed9bd9e7bcd05b3fe6
Owenhilll <owhi7342@colorado.edu>
2021-09-23 19:55:17 +0000

    Resolved merge

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,26 +13,6 @@ TicTacToe::TicTacToe(){
     }
   }
 }
-
-<<<<<<< HEAD
-void DisplayBoard(){
-    for(int i = 0; i < 3;i++){
-      for(int j= 0; j < 3; j++){
-        std::cout << TicTacToe[i][j];
-      }
-      std::cout << std::endl;
-    }
-}
-
-
-int main(){
-  //initializes tic tac toe baord with blank spaces in each section
-  CreateBoard();
-  //testing to see if board initialized
-  std::cout << TicTacToe[0][0] << std::endl;
-  //
-  return 0;
-=======
 // displays the 3x3 array Board[3[3]
 void TicTacToe::DisplayBoard(){
   for(int i=0;i<3;i++){
@@ -42,5 +22,4 @@ void TicTacToe::DisplayBoard(){
     }
   }
   std::cout<<"\n\n";
->>>>>>> 9038f11a715c2e3c95ad72fdba7b52bdb41befda
 }

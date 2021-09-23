3bc4e0fe4d175eafa92a9123c38ca3f43b6dcf8a
Owenhilll <owhi7342@colorado.edu>
2021-09-23 18:52:58 +0000

    Resolving merge conflict with master branch

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -12,6 +12,15 @@ void CreateBoard(){
   }
 }
 
+void DisplayBoard(){
+    for(int i = 0; i < 3;i++){
+      for(int j= 0; j < 3; j++){
+        std::cout << TicTacToe[i][j];
+      }
+      std::cout << std::endl;
+    }
+}
+
 
 int main(){
   //initializes tic tac toe baord with blank spaces in each section

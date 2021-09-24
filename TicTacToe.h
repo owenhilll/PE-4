2db31741fe8ddeff3ab68bc9e7daa7a738edd2ed
zachakin <roak1317@colorado.edu>
2021-09-24 02:11:41 -0600

    wrote PlaceMarker(), get_currentPlayer(), initPlayers(), and created Player and Position structs

--- TicTacToe.h
+++ TicTacToe.h
@@ -7,15 +7,31 @@
 #include <string>
 
 struct Player{
+    std::string name;
     std::string symbol;
+    int turn_count;
+    Player* next;
+    Player* prev;
+};
+
+struct Position{
+    int row;
+    int col;
 };
 
 
 class TicTacToe{
     public:
-        TicTacToe();            //constuctor, initializes board with empty squares
-        void DisplayBoard();    // displays the board
+        TicTacToe();                                            //constuctor, initializes board with empty squares
+        void initPlayers();                                     // sets player values, connects the players with a doubly linked list
+        void DisplayBoard();                                    // displays the board
+        bool PlaceMarker(Position pos, Player current, Player *&nextPlayer);  // Updates the output of the board
+        Player* get_currentPlayer();
+        bool gameOver;                                          // returns true if 9 turns have been taken   
     private:
         std::string Board[3][3];  // 3x3 array of strings
+        Player player1;           // X's
+        Player player2;           // O's
+        Player* currentPlayer;    // pointer to the current player
 };
 #endif
